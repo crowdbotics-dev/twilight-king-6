ad81b9124d932b88ab6300ba06233958c7bf9747
Crowdbotics <engineering@crowdbotics.com>
2024-03-25 21:48:38 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-king-6",
+  url: "https://twilight-king-6.botics.co",
+  api: "https://twilight-king-6.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
